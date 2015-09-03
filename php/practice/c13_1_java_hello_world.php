e3aa3f9cdaa19dca499f6debaefecc4ab77eff84
thienchuong84 <thienchuong84@gmail.com>
2015-09-03 17:20:15 +0700

    template at piv

---
+++ php/practice/c13_1_java_hello_world.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+	<title>Java Hello World</title>
+</head>
+<body>
+
+<script type="text/javascript"><!--
+	document.write("Hello World");
+// --></script>
+<noscript>
+	Your browsers doesn't support or has disable
+</noscript>
+
+<br>Vd 2 : <br>
+<script type="text/javascript">
+	status = "All system are working";
+	document.write(status);
+</script><br><br>
+
+String Contatenation: <br>
+<script type="text/javascript">
+	var messages = "test1\ttest2";
+	messages += " 2";
+	document.write("you have " + messages + " messages.\'")
+</script><br><br>
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html?
